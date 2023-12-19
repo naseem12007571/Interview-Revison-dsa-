ef1ef748d520103d3434efae45b5a85ad8a75537
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-19 11:57:30 +0530

    150 leetcode

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -17,4 +17,4 @@ public:
 // This is because the majority element occurs more than n/2 times, and when the array is sorted, it will occupy the middle position.
 
 // The code returns the element at index n/2 as the majority element.
-// The time complexity of this approach is O(n log n) since sorting an array of size n takes O(n log n) time.
+// The time complexity of this approach is O(n log n) since sorting an array of size n takes O(n log n) time.  

d7d449977cf51cea74ad8bd9a3ecb48bd1716252
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-20 16:07:47 +0530

    compilor with gfg code coder arrmy.cpp

--- Second largest element of array .cpp
+++ Second largest element of array .cpp
@@ -14,3 +14,26 @@ int main()
     cout<<"Scond largest alement "<<arr[n-2];
     
 }
+
+// gfg code senond largest element yahn per -1 retun kanra tha ager second largest nni mikla to esliye -1 liya INT_MIN nhi liya
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	     
+	    int larg= -1;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>larg)
+	        larg=arr[i];
+	    }
+	    int secondlarg=-1;
+	    for(int i=0;i<n;i++){
+	    if(arr[i]!=larg)       //yahn per check kia h mera larg element large mein aachuka hoga toh arya ka koi elemt eaula nhi hona chaiye eske large 
+	    secondlarg=max(secondlarg,arr[i]);    //yahn per jo mera arr bacha us mein us mera larg nikal de 
+	}
+	return secondlarg;
+  }
+};
+// logic coder arrmy https://www.youtube.com/watch?v=567332frcF0&list=PLQEaRBV9gAFvh2mVVhsph6OiemIHtp3r-&index=6

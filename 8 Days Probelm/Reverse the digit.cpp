9a0174c937507003719f2512f68c7e1e12ce1550
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-23 16:52:36 +0530

    coder army concept

---
+++ 8 Days Probelm/Reverse the digit.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans=0, rem;
+       while(x!=0){
+          rem=x%10;
+           x=x/10;
+           if(ans>INT_MAX/10){
+               return 0;
+           } 
+           if (ans<INT_MIN/10){
+             return 0;
+           }
+           ans=ans*10+rem;
+       }
+        
+        return ans;
+    }
+};
+
+        //  if(ans>INT_MAX/10){ this condition use for overflow ager number ye h       3456 or revere per ye 6543 banega to ye pahel se numbser jyda ho raha    32bit mein nhi aparaha to   lagai
+        //        return 0;
+        //    } 
+        //    if (ans<INT_MIN/10){
+        //      return 0;
+
+https://www.youtube.com/watch?v=0j7879JOgIU

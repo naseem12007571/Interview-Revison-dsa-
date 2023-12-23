d2f73db466581a209e8809855fbadb90e6b33fab
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-23 16:20:22 +0530

    coder army

--- 258. Add Digits.cpp
+++ 258. Add Digits.cpp
@@ -17,4 +17,6 @@ public:
 // rem= num%10;// reminder nikal liya  suppose 635%10 5 reminder fir rem 63%10 3r 6%10 rem 6 rem
 // num=num/10; // number jo bacha usko divide 63/10  6  6/10 =0 
 // ans=ans+rem; // 0+5=5  fir 5+3= 8 agya 5+3+6 14 fir num ki jahag 14 single digit meinn lana  h to first loop esliye num>9 se 
-//or num= ans; se jo asn ke ander 14 aaya use fir se proces ke liye first loop lagya kiyo ki ans single digit men lana h 
+//or num= ans; se jo asn ke ander 14 aaya use fir se proces ke liye first loop lagya kiyo ki ans single digit men lana h  
+
+https://www.youtube.com/watch?v=0j7879JOgIU

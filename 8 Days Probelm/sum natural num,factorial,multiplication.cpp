89107e8e740ee03f565c53a5a842c2f1365dffb4
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-25 16:23:58 +0530

    Create sum natural num,factorial,multiplication.cpp

---
+++ 8 Days Probelm/sum natural num,factorial,multiplication.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{    
+    // abcd print
+    // char name;
+    // for(name='a';name<='z';name++){
+    //     cout<<name<<"  ";
+    // }
+       /////// multiplication print 5 10 15 20 25 like
+    // int n;
+    // cin>>n;
+    // for(int i=1;i<=10;i++){
+    // //     cout<<n*i<<" ";
+    // }
+    
+     ////// sum of natural number ////
+    // int n;
+    // cin>>n;
+    // int sum=0;
+    
+    // for(int i=1;i<=n;i++){
+    //     sum=sum+i;
+    // }
+    // cout<<sum;
+    
+   // cout<<n*(n+1)/2;   //ese fomula se bhi nikal sakte h
+   
+   
+   ////////// facorial number//
+//   int n;
+//   cout<<"Enter the factorial number ";
+//   cin>>n;
+//   int fact=1;
+   
+//   for(int i=1;i<=n;i++){
+//       fact=fact*i;
+//   }
+//   cout<<"factorial is = "<<fact;
+}

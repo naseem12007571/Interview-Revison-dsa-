bbd6d51dd19bc49cf0aa06614d70a1209f08eaae
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-26 16:22:28 +0530

    love babbar lecture 21

--- 189. Rotate Array K place.cpp
+++ 189. Rotate Array K place.cpp
@@ -11,6 +11,6 @@ public:
 };
 // k place se shift kana h k=2 yani 2 se shift karna h
 // nums de rakha  n size 3 4 5 6 
-// formula i+k%n bal  i=0 0+2%4= 2 ab =num[i] i=0 tha to 2 per jo aya h eleement 5 aya h use 0 per shift kardo 5 3 4 6 aisi chailta rahega  
+// formula i+k%n bal  i=0 0+2%4= 2 ab =num[i] i=0 tha to 2 per jo aya h eleement 5 aya h use 0 per shift kardo 5 3 4 6 aisi chailta rahega   
 
 // love babbar sir ki ye viode mein pura samjhaya h https://www.youtube.com/watch?v=Z7_nMTHROZo&t=462s

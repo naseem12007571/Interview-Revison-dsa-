6c5f3ccbdef2f3860b6912b3cf642500103fa4f0
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-28 12:57:43 +0530

    Update 80. Remove Duplicates from Sorted Array II.cpp

--- 80. Remove Duplicates from Sorted Array II.cpp
+++ 80. Remove Duplicates from Sorted Array II.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& arr) {
-        
-
-         int i = 0;
-
-        // 'for' loop ka istemal 'j' ke liye kiya gaya hai, jo 0 se lekar 'arr.size() - 1' tak badhta hai.
-        for (int j = 0; j < arr.size(); ++j) {
-            // Shart check ki gayi hai: Agar 'i' 2 se kam hai YA agar current element 'arr[j]' do positions pehle ke element 'arr[i - 2]' se bada hai.
-            if (i < 2 || arr[j] > arr[i - 2]) {
-                // Agar shart sach hai, to 'arr[i]' ko current element 'arr[j]' ke barabar karen aur 'i' ko ek se badhayen.
-                arr[i++] = arr[j];
+    int removeDuplicates(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(j<2 || nums[i]>nums[j-2] ){
+                nums[j]=nums[i];
+                j++;
             }
         }
-
-        // Modified array ka size, jisme duplicates hata kar, return kiya jata hai.
-        return i;
+        return j;
     }
 };
+
+//   https://www.youtube.com/watch?v=t12ZvLJrguk&t=365s

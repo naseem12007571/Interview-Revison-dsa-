894d1d679c1ef4c7618aa512a1cbfb87c5589c67
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-31 12:40:12 +0530

    Update fibonacci number.cpp

--- Compilor code/fibonacci number.cpp
+++ Compilor code/fibonacci number.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-  int x;
-  cout<<"Enter the number of x ";
-  cin>>x;
-  
-  int last=0,previous=1,current;
-  
-  for(int i=2;i<=x;i++){
-      current=last+previous;
-      last=previous;
-      previous=current;
-  }
-  
-  cout<<"fibonaci number is "<<previous;
-  
- 
-  
+
+int main() {
+    int x;
+    cout << "Enter the number of x: ";
+    cin >> x;
+
+    int last = 0, previous = 1, current;
+
+    for (int i = 2; i <= x; i++) {
+        current = last + previous;
+        last = previous;
+        previous = current;
+    }
+
+    cout << "Fibonacci number is: " << previous;
+
+    return 0;
 }

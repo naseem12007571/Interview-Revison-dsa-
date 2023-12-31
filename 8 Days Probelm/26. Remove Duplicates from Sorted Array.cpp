e6989d8263bf2b3cc3c14f9758214b150b655f96
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-31 18:30:09 +0530

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 8 Days Probelm/26. Remove Duplicates from Sorted Array.cpp
+++ 8 Days Probelm/26. Remove Duplicates from Sorted Array.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
         int n=nums.size();
-        int j=1;          // zero index already unique hoga esliye 1 se start
-        for(int i=1;i<n;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[j]=nums[i];
-                j++;
-
-            }
+        int insertindex=1; // 1 se esliye chiala raha kiyo ki 0 index per 
+                                //already unique value hogi 
+        for(int i=1;i<n;i++){  // ye bhi same 1 se chaileha;
+        if(nums[i-1]!=nums[i]){  // ye check kar raha h ager mera i se piche 
+        nums[insertindex]=nums[i];// bala index equal nhi h to use insert index mein stor karo
+        insertindex++;     // ab se age badhana hoga kiyo ki uper i se insert kari value
+          } 
         }
-         
-         return j;
+             return insertindex; //ab last mein wahi last value agye hogi hogi array ki 
     }
 };
-
 // https://www.youtube.com/watch?v=BWihLgxbCjQ 

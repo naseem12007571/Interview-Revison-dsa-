351842060f6399364942f53653602ceb0ad691df
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-31 12:09:09 +0530

    Create fibonacci number.cpp

---
+++ Compilor code/fibonacci number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x;
+  cout<<"Enter the number of x ";
+  cin>>x;
+  
+  int last=0,previous=1,current;
+  
+  for(int i=2;i<x;i++){
+      current=last+previous;
+      last=previous;
+      previous=current;
+  }
+  
+  cout<<"fibonaci number is "<<previous;
+  
+ 
+  
+}

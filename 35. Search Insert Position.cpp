3ad91f69a891b514fa921dfac9a086cc2a4993e3
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-01-20 12:46:05 +0530

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+       int n=nums.size();
+        int start=0,end=n-1;
+       
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return start;
+        
+    }
+};
+// https://www.youtube.com/watch?v=q1mgS0ZtLBE&t=223s

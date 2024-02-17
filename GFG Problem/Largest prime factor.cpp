297c19b7e03c2cb33475138a67940d7994b3bec6
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-02-18 00:25:12 +0530

    Create Largest prime factor.cpp

---
+++ GFG Problem/Largest prime factor.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here
+        
+        int ans=0;
+        for(int i=2;i*i<=N;i++){
+            while(N%i==0){
+                ans=max(ans,i);
+                N/=i;
+            }
+        }
+        if(N>1)
+        ans=N;
+        return ans;
+    }
+};

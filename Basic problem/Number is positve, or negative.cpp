290e35bfa85d8baf52383022c2e0aad07fc88069
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-02-26 23:22:32 +0530

    Create Number is positve, or negative.cpp

---
+++ Basic problem/Number is positve, or negative.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter the number n ";
+  cin>>n;
+  
+  if(n>0){
+      cout<<"Number is positive ";
+  }
+  else if (n<0){
+      cout<<"Number is negative ";
+  }
+  
+  else {
+      cout<<"Neither positive or negative";
+  }
+    
+}

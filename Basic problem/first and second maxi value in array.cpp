8f3dc7aabc606dd4d4255eeb86c8dfc8154a1668
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-09 22:40:40 +0530

    Create first and second maxi value in array.cpp

---
+++ Basic problem/first and second maxi value in array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int num;
+    int max=INT_MIN;
+    int min= INT_MAX;
+    cout<<"Enter the size of array ";
+    cin>>num;
+    
+    cout<<"Enter the value of array ";
+    int arr[1000];
+    for(int i=0;i<num;i++){
+        cin>>arr[i];
+    }
+     
+     for(int i=0;i<num;i++){
+         if(arr[i]>max){
+             max= arr[i];
+         }
+     }
+  
+     for(int i=0;i<num;i++){
+         if(arr[i]!=max){
+             min= arr[i];
+         }
+     }
+     
+     cout<<" max value is "<<max<<endl;
+     cout<<" second max value is "<<min;
+    
+}

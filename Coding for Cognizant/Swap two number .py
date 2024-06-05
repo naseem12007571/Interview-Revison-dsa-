ef613565689b50fdbcc73fba43a7abcaded4f639
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-06-06 00:25:21 +0530

    Create Swap two number .py

---
+++ Coding for Cognizant/Swap two number .py
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=20,b=30,c;
+    
+     cout<<"without swap "<<a<<" "<<b<<endl;
+    
+    c=a;    // pahle a khali kara 
+    a=b;    // ab a mein b ki value dalde khali h toh
+    b=c;    // yahahn a ki value c mein dali thi toh use b mein dal de 
+     
+   cout<<"after swap "<<a<<" "<<b;
+    
+}
+
+
+
+pyhton 
+a=10
+b=20
+print("without swap number ",a ,b)
+
+c=a
+a=b
+b=c
+
+print("after swap ",a,b)
+    

2fdff0db78b66ecb8b0955db339a6bc7b0d606f5
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-06-22 00:29:46 +0530

    Update move zero .cpp

--- Basic problem/move zero .cpp
+++ Basic problem/move zero .cpp
@@ -1,35 +1,25 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main()
+{
     int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    
-    int arr[1000];
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout<<"Enter the number of n ";
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
     
-    int count = 0; 
-
-  
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != 0) {
-            arr[count++] = arr[i]; 
-        }
+    int i=0;
+    for(int j=0;j<n;j++){
+        if(arr[j]!=0){
+        swap(arr[j],arr[i]);
+        i++;
     }
-
-   
-    while (count < n) {
-        arr[count++] = 0;
     }
-
-
-    cout << "Array after moving zeros to the end: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-  
+    
 }

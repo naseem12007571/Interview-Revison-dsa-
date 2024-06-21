1851a93d2a5b41092688dd14215d3d9ab4d68085
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-06-22 00:49:16 +0530

    Update move zero .cpp

--- Basic problem/move zero .cpp
+++ Basic problem/move zero .cpp
@@ -23,3 +23,38 @@ int main()
     }
     
 }
+
+
+
+//// with function how can i do it 
+#include <bits/stdc++.h>
+using namespace std;
+
+void movezero(int arr[], int n, int i=0) {
+    for (int j = 0; j < n; j++) {
+        if (arr[j] != 0) {
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of n : ";
+    cin>>n;
+    
+    cout<<"Enter the array element ";
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    movezero(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    
+}
+
